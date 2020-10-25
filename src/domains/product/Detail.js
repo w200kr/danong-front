806fd387ab5daac45278ebe96d890428525259f5
Asin <w200kr@gmail.com>
2020-10-26 07:01:11 +0900

    make detail page roughly

--- src/domains/product/Detail.js
+++ src/domains/product/Detail.js
@@ -4,11 +4,14 @@ import classNames from "classnames";
 import { useForm } from "react-hook-form";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-import {Box, Container, Grid, InputAdornment, IconButton, List, ListItem, ListItemText, Link, Paper} from '@material-ui/core';
-import withWidth from '@material-ui/core/withWidth';
+import {Box, Container, Grid, InputAdornment, Button, IconButton, List, ListItem, ListItemText, Link, Paper, Typography, Divider} from '@material-ui/core';
 
 import { Apps, CloudDownload } from "@material-ui/icons";
 import SearchIcon from '@material-ui/icons/Search';
+
+import Rating from '@material-ui/lab/Rating';
+
+import Slider from "react-slick";
 
 import { RenderAfterNavermapsLoaded, NaverMap, Marker } from 'react-naver-maps'; // 패키지 불러오기
 
@@ -18,48 +21,60 @@ import Logo from 'components/Atoms/Logo/Logo.js'
 import FormTextField from 'components/Atoms/FormTextField/FormTextField.js'
 import Header from "components/Header/Header.js";
 import Footer from "components/Footer/Footer.js";
-import SearchBar from "./SearchBar.js";
-import ProductCard from "./ProductCard.js";
 
 
 import styles from "./Detail.style.js";
 
 const useStyles = makeStyles(styles);
 
-function ListItemLink(props) {
-  return <ListItem button component="a" {...props} />;
-}
-
-
 const Detail = (props)=> {
   const classes = useStyles();
-  const {history, width, ...rest } = props;
-  const maxWidth = 'xl';
+  const {history, ...rest } = props;
 
-  const { handleSubmit, errors, control } = useForm({
-    reValidateMode: 'onBlur'
-  });
+  // const { handleSubmit, errors, control } = useForm({
+  //   reValidateMode: 'onBlur'
+  // });
 
-  const baseControllerProps = {
-    control: control,
-    defaultValue: '',
-  }
+  // const baseControllerProps = {
+  //   control: control,
+  //   defaultValue: '',
+  // }
 
-  const products = [{
-    },{
-    },{
-    },{
-    },{
-    },{
-    },{
-    },{
-    },{
-    },{
-    },{
-    },{
-    },{
-    },{
-  }]
+
+  const settings = {
+    className: classes.slider,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
+  const [state, setState] = React.useState({ nav1: null, nav2: null });
+  const slider1 = React.useRef();
+  const slider2 = React.useRef();
+
+  React.useEffect(() => {
+    setState({
+      nav1: slider1.current,
+      nav2: slider2.current
+    });
+  }, []);
+
+  const { nav1, nav2 } = state;
+
+
+  // const slider1 = React.useRef();
+  // const slider2 = React.useRef();
+
+  // const [state, setState] = React.useState({
+  //   nav1: slider1.current, 
+  //   nav2: slider2.current,
+  // });
+
+  // const { nav1, nav2 } = state;
+  // const nav1 = slider1.current;
+  // const nav2 = slider2.current;
 
   return (
     <Box className={classes.root} component="div" display='flex' flexDirection="column">
@@ -67,36 +82,229 @@ const Detail = (props)=> {
         brand={
           <Logo />
         }
-        leftLinks={
-          <List className={classes.links}>
-          </List>
-        }
-        rightLinks={
-          <List className={classes.links}>
-            <ListItemLink onClick={()=>history.push('/')}>
-              <ListItemText primary="재료찾기" />
-            </ListItemLink>
-            <ListItemLink onClick={()=>history.push('/')}>
-              <ListItemText primary="관심목록" />
-            </ListItemLink>
-            <ListItemLink onClick={()=>history.push('/')}>
-              <ListItemText primary="재료등록" />
-            </ListItemLink>
-            <ListItemLink onClick={()=>history.push('/')}>
-              <ListItemText primary="내정보" />
-            </ListItemLink>
-            <ListItemLink onClick={()=>history.push('/login')}>
-              <ListItemText primary="로그인" />
-            </ListItemLink>
-          </List>
-        }
       />
-      <Grid className={classes.container} container>
-        kjdfk
-      </Grid>
+      <Container className={classes.container} component={Grid} maxWidth='md' container
+        justify="center"
+        alignItems="center"
+      >
+        <Grid className={classes.summary} item xs={12} container>
+          <Grid item xs={7}>
+            <Box display='flex' flexDirection='column' justifyContent='center'>
+              <Slider
+                asNavFor={nav2}
+                ref={slider => (slider1.current = slider)}
+              >
+                <div>
+                  <img src="https://dummyimage.com/410x410/d3d3d3/000000" alt="" />
+                </div>
+                <div>
+                  <img src="https://dummyimage.com/410x410/d3d3d3/000000" alt="" />
+                </div>
+                <div>
+                  <img src="https://dummyimage.com/410x410/d3d3d3/000000" alt="" />
+                </div>
+                <div>
+                  <img src="https://dummyimage.com/410x410/d3d3d3/000000" alt="" />
+                </div>
+                <div>
+                  <img src="https://dummyimage.com/410x410/d3d3d3/000000" alt="" />
+                </div>
+                <div>
+                  <img src="https://dummyimage.com/410x410/d3d3d3/000000" alt="" />
+                </div>
+              </Slider>
+              <Slider
+                asNavFor={nav1}
+                ref={slider => (slider2.current = slider)}
+                slidesToShow={4}
+                swipeToSlide={true}
+                focusOnSelect={true}
+              >
+                <div>
+                  <img src="https://dummyimage.com/410x410/d3d3d3/000000" width='110' height='80' alt="" />
+                </div>
+                <div>
+                  <img src="https://dummyimage.com/410x410/d3d3d3/000000" width='110' height='80' alt="" />
+                </div>
+                <div>
+                  <img src="https://dummyimage.com/410x410/d3d3d3/000000" width='110' height='80' alt="" />
+                </div>
+                <div>
+                  <img src="https://dummyimage.com/410x410/d3d3d3/000000" width='110' height='80' alt="" />
+                </div>
+                <div>
+                  <img src="https://dummyimage.com/410x410/d3d3d3/000000" width='110' height='80' alt="" />
+                </div>
+                <div>
+                  <img src="https://dummyimage.com/410x410/d3d3d3/000000" width='110' height='80' alt="" />
+                </div>
+                <div>
+                  <img src="https://dummyimage.com/410x410/d3d3d3/000000" width='110' height='80' alt="" />
+                </div>
+              </Slider>
+
+            </Box>
+          </Grid>
+          <Grid item xs={5}>
+            <Typography variant='h4'>
+              부안 우리네 쌀 방앗간 <span>인증</span>
+            </Typography>
+            <Typography variant='h5'>
+              전북 부안군 신기리 128
+            </Typography>
+            <Box component='span' display='flex'>
+              <Typography variant='subtitle1'>
+                별점
+              </Typography>
+              <Divider orientation="vertical" flexItem />
+              <Typography variant='subtitle1'>
+                하트
+              </Typography>
+            </Box>
+
+            <Typography variant='h4'>
+              전국 백미쌀 부문 15위
+            </Typography>
+
+            <Typography variant='h4'>
+              전북 백미쌀 부문 15위
+            </Typography>
+
+            <Typography variant='h5'>
+              198,000원
+            </Typography>
+            <Typography variant='h5'>
+              
+            </Typography>
+            <Typography variant='subtitle1'>
+              
+            </Typography>
+
+
+            <Box component='span' display='flex'>
+              <Typography variant='subtitle1'>
+                쌀 10KG X 12개월
+              </Typography>
+              <Box>
+                <span>하트</span><span>공유</span>
+              </Box>
+            </Box>
+            <Divider className={classes.horizontalDivider} />
+            <select name="" id="">
+              <option value="s">ss</option>
+            </select>
+            <Divider className={classes.horizontalDivider} />
+            <Typography variant='subtitle1'>
+              원산지 - 상세설명 꼭 참조해보세요!
+            </Typography>
+            <Divider className={classes.horizontalDivider} />
+            <Button variant="contained" fullWidth style={{backgroundColor: '#39290F', color:'white'}}>
+              구매하기
+            </Button>
+            <Button variant="contained" fullWidth style={{backgroundColor: '#39290F', color:'white'}}>
+              장바구니
+            </Button>
+          </Grid>
+        </Grid>
+      </Container>
+
+      <Box className={classes.tabs} display='flex' alignItems='center' justifyContent='center'>
+        <List className={classes.links}>
+          <ListItem>
+            <ListItemText primary="재료찾기" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="관심목록" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="재료등록" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="내정보" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="로그인" />
+          </ListItem>
+        </List>
+      </Box>
+      <Container className={classes.detail} component={Grid} maxWidth='sm' container
+        display='row'
+        justify="center"
+        alignItems="center"
+      >
+        <Grid className={classes.sellerInfo} item xs={12}>
+          <Box display='flex' flexDirection='column' justifyContent='flex-start' alignItems='center'>
+            <Typography>
+              농업인 정보
+            </Typography>
+            <img src="https://dummyimage.com/180x180/d3d3d3/000000" width='180' height='180' alt="" />
+            <Typography>
+              이수민
+            </Typography>
+            <Typography>
+              부안 우리네 쌀 방앗간 대표
+            </Typography>
+
+            <Box component="fieldset" mb={3} borderColor="transparent">
+              <Rating id='dd' name="read-only" value={2} readOnly />
+              <Typography component="label" htmlFor='dd'>(4.9 / 352)</Typography>
+            </Box>
+            <Box display='flex' width="50%" justifyContent='space-evenly'>
+              <Button variant="contained">Tel. 043-123-1234</Button>
+              <Button variant="contained">1:1 문의</Button>
+            </Box>
+          </Box>
+          <Typography>
+            다농 등록건수 : 1건<span style={{width:100}}></span>주 판매 작물 : 곡류
+          </Typography>
+          <Box className={classNames(classes.roundBox)}>
+            <Typography variant='h6' align='center'>주요 경력</Typography>
+            <Typography align='center'>쌀 농사 경력 37년</Typography>
+            <Typography align='center'>해당 농지 농사 경력 21년</Typography>
+            <Typography align='center'>2003년 7월 친환경 인증 농업인 취득</Typography>
+          </Box>
+          <Box className={classNames(classes.roundBox)}>
+            <Typography align='center'>농업인 한마디</Typography>
+            <pre>
+              {`평생을 농사지어 세 자식을 키우고 먹이고 
+              대학까지 보냈습니다. 
+              쌀 농사에 일생을 바친만큼 
+              쌀은 자신있습니다. 
+              믿고 구매해주십시오 감사합니다
+              (1:1문의는 자주 확인이 불가하오니 전화주시면 감사하겠습니다.)`}
+            </pre>
+          </Box>
+        </Grid>
+        <Grid className={classes.productInfo} item xs={12}>
+        </Grid>
+        <Grid className={classes.soilInfo} item xs={12}>
+          <div dangerouslySetInnerHTML={{__html: ``.replace(/(<? *script)/gi, 'illegalscript')}} >
+          </div>
+        </Grid>
+        <Grid className={classes.reviews} item xs={12}>
+          <Box display='flex' flexDirection="column">
+            <Box display='flex' >
+              <img src="https://dummyimage.com/40x40/d3d3d3/000000" height={40} width={40} alt="" />
+              <Box display='flex' flexDirection="column">
+                <Typography>
+                  신석은
+                </Typography>
+                <Box component="fieldset" borderColor="transparent" m={0} p={0}>
+                  <Rating id='dd' name="read-only" value={2.5} readOnly />
+                  <Typography component="label" htmlFor='dd'>2020.10.24</Typography>
+                </Box>
+              </Box>
+            </Box>
+            <Typography>
+              항상 잘먹고 있습니다 감사합니다
+            </Typography>
+          </Box>
+        </Grid>
+      </Container>
+      <Footer />
     </Box>
   );
 };
 
 // export default Search;
-export default withWidth()(Detail);
+export default Detail;
--- src/domains/product/Detail.style.js
+++ src/domains/product/Detail.style.js
@@ -1,17 +1,54 @@
 const searchStyle = theme => {
   return ({
     root:{
-    },
-    // offset: theme.mixins.toolbar,
-    links: {
-      display: 'flex',
-      // justifyContent: 'flex-end',
-
-      '& *':{
-        width: 'auto',
-      }
+      // display: 'flex',
+      // flexDirection: 'row',
     },
     container: {
+      // backgroundColor: 'yellow',
+      // flex: '1 1 auto',
+      // display: 'flex',
+      // flexWrap: 'nowrap',
+      // height: '100vh',
+    },
+    horizontalDivider: {
+      marginTop: 40,
+      marginBottom: 40,
+    },
+    summary: {
+      // height: '100vh',
+      // backgroundColor: 'yellow'
+    },
+    slider: {
+      height: 410,
+      width: 410,
+    },
+    tabs: {
+      width: '100vw',
+      height: 50,
+      backgroundColor: '#707070',
+    },
+    links: {
+      display: 'flex'
+    },
+    detail: {
+    },
+    roundBox: {
+      height: 100,
+      borderRadius: 20,
+      border: '1px solid black',
+    },
+    sellerInfo: {
+
+    },
+    productInfo: {
+
+    },
+    soilInfo: {
+
+    },
+    reviews: {
+      marginTop: 100,
     },
   })
 }
